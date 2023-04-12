1381101a4a77e678dfdbe8cdb0c8eac3f91b68e9
kenneth-alt <kenneth.ugo15@gmail.com>
2023-04-12 00:45:02 -0600

    working checkpoint

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,9 +1,38 @@
+import { useState } from "react"
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Morning Meeting',
+        day: 'March 5th at 8:30am',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Interview Prep',
+        day: 'March 11th at 7:30pm',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Market Run',
+        day: 'March 15th at 11:30am',
+        reminder: false,
+    },
+]) 
+
+// Delete Task
+const deleteTask = (id) => {
+  console.log('delete', id)
+}
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }
