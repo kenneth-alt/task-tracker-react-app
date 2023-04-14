882cd1744ddd452020f77233da8b97e84d6837d6
kenneth-alt <kenneth.ugo15@gmail.com>
2023-04-14 02:25:56 -0600

    add update reminder functionality

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -23,10 +23,17 @@ const App = () => {
 
     return data
   }
+  // Fetch One Task by id
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`)
+    const data = await res.json()
+
+    return data
+  }
 
 // Add Task
 const addTask = async (task) => {
-  const res = await fetch('http://localhost:5000/tasks', {method: 'POST', headers: {'content-type': 'application/json'}, body: JSON.stringify(task)}) 
+  const res = await fetch('http://localhost:5000/tasks', {method: 'POST', headers: {'Content-type': 'application/json'}, body: JSON.stringify(task)}) 
 
   const data = await res.json()
 
@@ -45,8 +52,15 @@ const deleteTask = async (id) => {
 }
 
 // Toggle Reminder
-const toggleReminder = (id) => {
-  setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+const toggleReminder = async (id) => {
+  const taskToToggle = await fetchTask(id) 
+  const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder }
+
+  const res = await fetch(`http://localhost:5000/tasks/${id}`, {method: 'PUT', headers: {'Content-type': 'application/json'}, body: JSON.stringify(updTask)})
+
+  const data = await res.json()
+
+  setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: data.reminder } : task))
 }
 
   return (
