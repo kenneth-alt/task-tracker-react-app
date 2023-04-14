121cd3af1c47a5fc753a938529e9e97632504f7f
kenneth-alt <kenneth.ugo15@gmail.com>
2023-04-14 02:07:55 -0600

    connect frontend to server

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
@@ -7,15 +7,40 @@ const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([]) 
 
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+
+    getTasks()
+  }, [])
+
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
+
 // Add Task
-const addTask = (task) => {
-  const id = Math.floor(Math.random() * 10000) + 1
-  const newTask = { id, ...task }
-  setTasks([...tasks, newTask])
+const addTask = async (task) => {
+  const res = await fetch('http://localhost:5000/tasks', {method: 'POST', headers: {'content-type': 'application/json'}, body: JSON.stringify(task)}) 
+
+  const data = await res.json()
+
+  setTasks([...tasks, data])
+
+  //const id = Math.floor(Math.random() * 10000) + 1
+  //const newTask = { id, ...task }
+  //setTasks([...tasks, newTask])
 }
 
 // Delete Task
-const deleteTask = (id) => {
+const deleteTask = async (id) => {
+  await fetch(`http://localhost:5000/tasks/${id}`, {method: 'DELETE'})
+
   setTasks(tasks.filter((task) => task.id !== id))
 }
 
