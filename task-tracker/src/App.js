e4d3a297511e9136a729a027bf2a1049ca4e666a
kenneth-alt <kenneth.ugo15@gmail.com>
2023-04-14 01:39:25 -0600

    move tasks mock data to db.json

--- task-tracker/src/App.js
+++ task-tracker/src/App.js
@@ -5,38 +5,7 @@ import AddTask from "./components/AddTask";
 
 const App = () => {
   const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([
-    {
-        id: 1,
-        text: 'Morning Meeting',
-        day: 'March 5th at 8:30am',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Interview Prep',
-        day: 'March 11th at 7:30pm',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Market Run',
-        day: 'March 15th at 11:30am',
-        reminder: false,
-    },
-    {
-      id: 4,
-      text: 'Learn coding',
-      day: 'March 17th at 7:30pm',
-      reminder: false,
-  },
-  {
-    id: 5,
-    text: 'Go hiking',
-    day: 'March 20th at 9:30am',
-    reminder: false,
-},
-]) 
+  const [tasks, setTasks] = useState([]) 
 
 // Add Task
 const addTask = (task) => {
